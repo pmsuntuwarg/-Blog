2719d7072cdefd1dc843f55392a06824677bab5e
Mahesh <msunuwar2@live.com>
2020-01-29 00:27:12 +0545

    publish branch

---
+++ wwwroot/assets/js/Module/page.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    var pageListDT = $("#pageListDT").DataTable({
+        processing: true,
+        serverSide: true,
+        ajax      : {
+            url     : "/admin/page/getpages",
+            type    : "post",
+            dataType: "json"
+        },
+        language: {
+            search           : "",
+            searchPlaceholder: "Search.."
+        },
+        columns: [
+            { data: "pageName", name: "Page Name", searchable: false, autoWidth: true },
+            { data: "title", name: "Page Title", searchable: false, autoWidth: true },
+            {
+                data  : "id",
+                render: (data, type, row, meta) => `<a href="Page/Detail/${data}"> Detail </a> <a href="Page/Update/${data}"> Edit </a>`
+            }
+        ]
+    });
+});
---
+++ wwwroot/assets/js/page/login.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    $('#login-form').validate({
+        errorClass: "help-block",
+        rules: {
+            Email: {
+                required: true,
+                email: true
+            },
+            Password: {
+                required: true,
+                password: true
+            }
+        },
+        highlight: function (e) {
+            $(e).closest(".form-group").addClass("has-error");
+        },
+        unhighlight: function (e) {
+            $(e).closest(".form-group").removeClass("has-error");
+        }
+    });
+});
